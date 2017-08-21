38b1d5833deff5df030ff1af6f8a311ad04ef995
ting <1290549530@qq.com>
2017-08-21 19:01:27 +0800

    less-loader

---
+++ less-loader-master/src/processResult.js
@@ -0,0 +1,50 @@
+const removeSourceMappingUrl = require('./removeSourceMappingUrl');
+const formatLessError = require('./formatLessError');
+
+/**
+ * Removes the sourceMappingURL from the generated CSS, parses the source map and calls the next loader.
+ *
+ * @param {loaderContext} loaderContext
+ * @param {Promise<LessResult>} resultPromise
+ */
+function processResult(loaderContext, resultPromise) {
+  const { callback } = loaderContext;
+
+  resultPromise
+    .then(({ css, map, imports }) => {
+      imports.forEach(loaderContext.addDependency, loaderContext);
+      return {
+        // Removing the sourceMappingURL comment.
+        // See removeSourceMappingUrl.js for the reasoning behind this.
+        css: removeSourceMappingUrl(css),
+        map: typeof map === 'string' ? JSON.parse(map) : map,
+      };
+    }, (lessError) => {
+      throw formatLessError(lessError);
+    })
+    .then(({ css, map }) => {
+      callback(null, css, map);
+    }, callback);
+}
+
+module.exports = processResult;
+
+
+
+var createElement = (function(){
+    var oDiv;
+    if( div != undefined ){
+        var oDiv = document.createElement("div");
+        oDiv.innerHTML = "我是个弹窗";
+        oDiv.style.display = "none";
+    }
+    return oDiv;
+})();
+
+
+document.body.onclick(function(){
+    var newDiv = new createElement();
+    newDiv.style.display = 'block';
+})
+
+
---
+++ less-loader-master/test/helpers/compile.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const fixturePath = path.resolve(__dirname, '..', 'fixtures');
+const outputPath = path.resolve(__dirname, '..', 'output');
+
+function compile(fixture, moduleRules, resolveAlias = {}) {
+  return new Promise((resolve, reject) => {
+    const entry = path.resolve(fixturePath, 'less', `${fixture}.less`);
+
+    webpack({
+      entry,
+      output: {
+        path: outputPath,
+        filename: 'bundle', // omitting the js extension to prevent jest's watcher from triggering
+      },
+      module: {
+        rules: moduleRules,
+      },
+      resolve: {
+        alias: resolveAlias,
+      },
+    }, (err, stats) => {
+      const problem = err || stats.compilation.errors[0] || stats.compilation.warnings[0];
+
+      if (problem) {
+        const message = typeof problem === 'string' ? problem : 'Unexpected error';
+        const error = new Error(problem.message || message);
+
+        error.originalError = problem;
+        error.stats = stats;
+
+        reject(error);
+
+        return;
+      }
+
+      resolve(stats);
+    });
+  });
+}
+
+module.exports = compile;
